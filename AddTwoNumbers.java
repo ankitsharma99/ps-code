e96dc52a0be7a59f68ac8340f86c511d8ec4e47a
Ankit Sharma <sharmankit9593@gmail.com>
2023-03-24 10:10:42 +0530

    removed unwanted files

--- AddTwoNumbers.java
+++
@@ -1,7 +0,0 @@
-public class AddTwoNumbers {
-    public static void main(String[] args) {
-        int first = Integer.valueOf(args[0]);
-        int second = Integer.valueOf(args[1]);
-        System.out.println(first + second);
-    }
-}
