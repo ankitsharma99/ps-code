62722e5415357cee623ab2e41b0b47803798411d
Ankit Sharma <ankit.sharma9@publicissapient.com>
2023-04-24 16:15:08 +0530

    updated codes with Fork/Join Pool

--- fourth-week/src/secondsession/fourthquestion/RecursiveActionFibonacci.java
+++ fourth-week/src/secondsession/fourthquestion/RecursiveActionFibonacci.java
@@ -3,40 +3,41 @@ package secondsession.fourthquestion;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
-
 class FibonacciTask extends RecursiveAction {
-    private int n;
-    private long[] fib;
+    private final int n;
+    private long result;
 
-    public FibonacciTask(int n, long[] fib) {
+    public FibonacciTask(int n) {
         this.n = n;
-        this.fib = fib;
+    }
+
+    public long getResult() {
+        return result;
     }
 
     @Override
     protected void compute() {
         if (n <= 1) {
-            fib[n] = n;
-            return;
+            result = n;
+        } else {
+            FibonacciTask leftTask = new FibonacciTask(n - 1);
+            FibonacciTask rightTask = new FibonacciTask(n - 2);
+            leftTask.fork();
+            rightTask.compute();
+            leftTask.join();
+            result = leftTask.getResult() + rightTask.getResult();
         }
-
-        FibonacciTask left = new FibonacciTask(n - 1, fib);
-        FibonacciTask right = new FibonacciTask(n - 2, fib);
-
-        invokeAll(left, right);
-
-        fib[n] = fib[n - 1] + fib[n - 2];
     }
+
+
 }
 
 public class RecursiveActionFibonacci {
     public static void main(String[] args) {
-        int n = 6;
-        long[] fib = new long[n + 1];
-
-        FibonacciTask task = new FibonacciTask(n, fib);
-        task.invoke();
-
-        System.out.println("Fibonacci(" + n + ") = " + fib[n]);
+        int n = 10;
+        FibonacciTask task = new FibonacciTask(n);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(task);
+        System.out.println("The Fibonacci number at position " + n + " is " + task.getResult());
     }
 }
